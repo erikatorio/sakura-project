a3190ee48bd26988259806fcafe69dc0509deecd
Erika Torio <34079520+erikatorio@users.noreply.github.com>
2020-01-10 11:43:32 +0800

    added get data from firestore functionality
    integration with front-end

--- public/app.js
+++ public/app.js
@@ -74,3 +74,41 @@ $(document).ready(function() {
       
   });
 });
+
+//get graph data
+async function getStats() {
+    var groups = ['Group 1', 'Group 2', 'Group 3'];
+
+    var data = [];
+    var current_group_id;
+    for(var i = 0; i < groups.length; i++) {
+        var current_group = await db.collection('stats').doc('stats').collection(groups[i]);
+
+        var query = await current_group.get().then(function(snapshot) {
+            var current = groups[i];
+            var values = [];
+            snapshot.forEach(function(doc) {
+                //console.log(doc.id, "=>", doc.data(), "=>", doc.data().count);
+                var x = {
+                    key: doc.id,
+                    count: doc.data().count
+                }
+
+                values.push(x);
+            });
+            
+            data.push({
+                key: groups[i],
+                info: values
+            });
+        });
+    }
+    return data;
+}
+
+//display data
+async function displayData() {
+    var x = await getStats().then(function(y) {
+        //to do, integrate data from firestore with graph
+    }); 
+}
--- public/test.js
+++ public/test.js
@@ -11,14 +11,53 @@ firebase.initializeApp({
 
 var db = firebase.firestore();
 
-var users = db.collection('users');
-      var query = users.get().then(function(querySnapshot) {
-          querySnapshot.forEach(function(doc) {
-              // doc.data() is never undefined for query doc snapshots
-              console.log(doc.id, " => ", doc.data());
-              console.log(doc.data().group);
-          });
-      })
-      .catch(function(error) {
-          console.log("Error getting documents: ", error);
-      });
+// var users = db.collection('users');
+//       var query = users.get().then(function(querySnapshot) {
+//           querySnapshot.forEach(function(doc) {
+//               // doc.data() is never undefined for query doc snapshots
+//               console.log(doc.id, " => ", doc.data());
+//               console.log(doc.data().group);
+//           });
+//       })
+//       .catch(function(error) {
+//           console.log("Error getting documents: ", error);
+//       });
+
+async function getStats() {
+    var groups = ['Group 1', 'Group 2', 'Group 3'];
+
+    var data = [];
+    var current_group_id;
+    for(var i = 0; i < groups.length; i++) {
+        var current_group = await db.collection('stats').doc('stats').collection(groups[i]);
+
+        var query = await current_group.get().then(function(snapshot) {
+            var current = groups[i];
+            var values = [];
+            snapshot.forEach(function(doc) {
+                //console.log(doc.id, "=>", doc.data(), "=>", doc.data().count);
+                var x = {
+                    key: doc.id,
+                    count: doc.data().count
+                }
+
+                values.push(x);
+            });
+            
+            data.push({
+                key: groups[i],
+                info: values
+            });
+        });
+    }
+    return data;
+}
+
+async function test() {
+    var x = await getStats().then(function(y) {
+        console.log(y);
+    }); 
+}
+
+test();
+
