d5f603f2289efb195bb9e8ae32d4e36f1198272f
apsolinap <38276252+apsolinap@users.noreply.github.com>
2020-01-20 14:03:14 +0800

    Add files via upload

---
+++ public/data_summary.js
@@ -0,0 +1,47 @@
+google.charts.load("current", {packages:["corechart"]});
+  google.charts.setOnLoadCallback(function(){
+        for(var i = 1;i<4;i++){
+          drawChart(i);
+        }
+      });
+      function drawChart(group) {
+        var data = google.visualization.arrayToDataTable([
+          ['Category', 'Number of Reports'],
+          ['Category A', 11],
+          ['Category B', 6],
+          ['Category C', 4],
+          ['Category D', 9]
+        ]);
+
+        var options = {
+          title: 'Reports by Group '+group,
+          pieHole: 0.4,
+          legend: 'right',
+          width:390,
+          height:350,
+          label: {color:'#F4989C'},
+          colors: ["#EBD2B4", "#F4989C", "#DAC4F7", "#ACECF7"]
+        };
+        var target = 'donutchart-'+group;
+        var chart = new google.visualization.PieChart(document.getElementById(target));
+        chart.draw(data, options);
+      }
+
+  window.onafterprint = afterPrint;
+  //hides print button when clicked
+  function printSummary(){
+    $("#print-btn").css("display", 'none');
+    $("#shape").css("display", 'inline');
+    setTimeout(function(){
+      $("#shape").css("display", 'none');
+      window.print()
+    },1000) ;
+  }
+
+  //displays print button after printing/closing
+  function afterPrint(){
+    setTimeout(function(){
+      $("#print-btn").css("display", 'inline');
+      $("#shape").css("display", 'none');
+    },2000) ;
+  }
