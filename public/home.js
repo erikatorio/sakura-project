f51d96755affec44099ef17fb5b38529d4a5d828
sshrbnc <38273738+sshrbnc@users.noreply.github.com>
2020-01-21 14:40:19 +0800

    fixed confirmation modal

--- public/home.js
+++ public/home.js
@@ -20,3 +20,32 @@ function selectCategory(elem) {
     elem.classList.add('active');
   }
 }
+
+document.querySelector("#confirm_btn2").addEventListener("click", function() {
+  swal({
+    text: "Report has been sent!",
+    type: 'success',
+    position: "top right",
+    backdrop: "transparent",
+    background: "#efffed",
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    allowEnterKey: false,
+    showConfirmButton: false,
+    showCancelButton: false,
+    timer: 2000
+  });
+  setTimeout(function(){$('#submit_modal1').css("filter", "blur(0)");}, 500);
+});
+
+$("#confirm_btn1").click(function(){
+  $("#submit_modal1").css("filter", "blur(2px)");  
+});
+
+$("#cancel_btn").click(function(){
+  $("#submit_modal1").css("filter", "blur(0)");  
+});
+
+function showChat(){
+  $('.toast').toast('show');
+}
